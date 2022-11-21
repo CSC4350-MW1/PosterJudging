db7ec99cd32615e1dc87034f6cab803fa2533658
razatsutradhar <razat74@gmail.com>
2022-11-20 20:24:36 -0500

    created Grader, Score, and Project classes and implemented creating new grading assignments

--- Code/grading_assignment.py
+++ Code/grading_assignment.py
@@ -1,32 +1,54 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import json
+from judging import *
+import pandas as pd
+import random
 
 # Use a service account.
-cred = credentials.Certificate('Code\key.json')
+cred = credentials.Certificate('Code/key.json')
 app = firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
+#clear current grading assignments
+clear_grading_assignments(db)
+
 # Grading
-grading = db.collection(u'Grading')
-grading_data = grading.get()
+graders_db = db.collection(u'Graders')
+grader_data = graders_db.get()
 
-print("grading:")
-for data in grading_data:
-    print(data.to_dict())
+graders = dict()
+for grader in grader_data:
+    g = Grader(**json.loads(json.dumps(grader.to_dict())))
+    graders[g.grader_id] = g
+
+# Projects
+projects_db = db.collection(u'Projects')
+projects_data = projects_db.get()
+
+projects = dict()
+for data in projects_data:
+    p = Project(**json.loads(json.dumps(data.to_dict())))
+    projects[p.project_id] = p
 
 
-# Project
-project_data = db.collection(u'Projects').get()
+grading_queue =list(graders.keys())
+random.shuffle(grading_queue)
 
-print("\nprojects:")
-for data in project_data:
-    print(data.to_dict())
+for project in projects:
+    grading_stack = []
+    for i in range(2):
+        while(len(grading_queue) > 0 and grading_queue[0] in graders and projects[project].mentor == graders[grading_queue[0]].grader_id):
+            grading_stack.append(grading_queue.pop(0))
+        if (projects[project].mentor not in graders[grading_queue[0]].grader_id and project not in graders[grading_queue[0]].assigned_projects):
+            graders[grading_queue[0]].assigned_projects.append(project)
+            grading_queue.append(grading_queue.pop(0))
+    grading_queue = grading_stack + grading_queue
 
-# Scores
-scores_data = db.collection(u'Scores').get()
 
-print("\nscores:")
-for data in scores_data:
-    print(data.to_dict())
+for grader in graders:
+    graders[grader].upload(db)
+
+firebase_admin.delete_app(app)
---
+++ Code/judging.py
@@ -0,0 +1,115 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import json
+
+
+class Project:
+    def __init__(self, mentor, project_title, student_emails, student_names, student_ids, is_undergrad=True, project_id=None, score=0):
+        self.mentor = mentor
+        self.project_title = project_title
+        self.student_emails = student_emails
+        self.student_names = student_names
+        self.student_ids = student_ids
+        self.score = score
+        self.is_undergrad = is_undergrad
+
+        if project_id == None:
+            self.project_id = self.generate_id()
+        else:
+            self.project_id = project_id
+
+    # Generates a unique id for the project
+    def generate_id(self):
+        return str(self.project_title+'-'+self.student_names[0].replace(' ', '_'))
+
+    # Returns the project as a JSON object
+    def to_json(self):
+        return json.dumps(self.__dict__)
+
+    # Uploads the project to the database
+    def upload(self, db):
+        json_data = json.dumps(self.__dict__)
+        db.collection(u'Projects').document(self.project_id).set(json.loads(json_data))
+
+    # Cumulates the scores of the project
+    def add_scores(self, scores):
+        final_score = 0
+        num_of_scores = len(scores)
+        for score in scores:
+            final_score += sum(score)
+        self.score = final_score/num_of_scores
+        return self.score
+
+        
+
+class Score:
+    def __init__(self, project_id, grader_id, score=[1,1,1,1,1,1,1], comments='No Comments', score_id=None):
+        self.project_id = project_id
+        self.grader_id = grader_id
+        self.score = score
+        self.comments = comments
+        if(score_id == None):
+            self.score_id = self.generate_id()
+        else:
+            self.score_id = score_id
+    
+    # Generates a unique id for the score
+    def generate_id(self):
+        return str(self.project_id+'-'+self.grader_id)
+
+    # Returns the score as a JSON object
+    def to_json(self):
+        return json.dumps(self.__dict__)
+    
+    # Uploads the score to the database
+    def upload(self, db):
+        json_data = json.dumps(self.__dict__)
+        db.collection(u'Scores').document(self.score_id).set(json.loads(json_data))
+    
+
+
+class Grader:
+    def __init__(self, grader_id, grader_name, assigned_projects=[]):
+        self.grader_id = grader_id
+        self.grader_name = grader_name
+        self.assigned_projects = assigned_projects
+
+    # Returns the grader as a JSON object
+    def to_json(self):
+        return json.dumps(self.__dict__)
+    
+    # Uploads the grader to the database
+    def upload(self, db):
+        json_data = json.dumps(self.__dict__)
+        db.collection(u'Graders').document(self.grader_id).set(json.loads(json_data))
+    
+    #clear grading assignments
+    def clear_grading(self, db):
+        self.assigned_projects = list()
+        self.upload(db)
+
+    def __str__(self):
+        return str(self.grader_name + " " + self.grader_id + " " + str(self.assigned_projects))
+
+
+
+
+def score_all_projects(scores, project_dict):
+    project_scores = dict()
+    for score in scores:
+        if score.project_id in project_scores:
+            project_scores[score.project_id].append(score.score)
+        else:
+            project_scores[score.project_id] = [score.score]
+    
+    key_intersection = set(project_dict.keys()).intersection(set(project_scores.keys()))
+    for project_id in key_intersection:
+        project_dict[project_id].add_scores(project_scores[project_id])
+
+def clear_grading_assignments(db):
+    graders_db = db.collection(u'Graders')
+    graders_data = graders_db.get()
+    for data in graders_data:
+        grader = Grader(**json.loads(json.dumps(data.to_dict())))
+        grader.clear_grading(db)
